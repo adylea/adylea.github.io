d0fd0852a48d6e6a2474179879897dd33468ced3
Adibah Solehah <adylea.solehah@gmail.com>
2021-10-04 20:21:00 +0800

    added geolocation haversine

---
+++ geolocation/js/haversine_main.js
@@ -0,0 +1,34 @@
+function calculateDistance(lat, lon){
+    let dpulze = haversine(lat,lon,2.922561,101.650965)
+    let pyramid = haversine(lat,lon,3.073065,101.607787)
+    let klcc = haversine(lat,lon,3.158761,101.714524)
+
+    return [dpulze,pyramid,klcc]
+}
+
+let ellocate = document.getElementById("locate")
+ellocate.addEventListener("click",function(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position){
+            let ellat = document.getElementById("lat")
+            let ellong = document.getElementById("long")
+            let elDpulze = document.getElementById("depulze")
+            let elklcc = document.getElementById("klcc")
+            let elsunway = document.getElementById("sunway")
+
+            let userLat = postion.coords.latitude
+            let userLong = position.coords.longitude
+
+            let distance = calculateDistances(userLat, userLong)
+
+            ellat.innerHTML = "Your latitude: " + userLat
+            ellong.innerHTML = "Your longitude " + userLong
+            elDpulze.innerHTML = "Distance to Dpulze, Cyberjaya is " + distances[0] + "km"
+            elsunway.innerHTML = "Distance to Sunway Pyramid is " + distances[1] + "km"
+            
+        })
+    }else{
+            alert("Geolocation is not supported!")
+        }
+    
+})
