e71c650a7e7e529de771c8566d2fffdf5a5b9008
Adibah Solehah <adylea.solehah@gmail.com>
2021-10-04 20:30:53 +0800

    changes

--- geolocation/js/haversine_main.js
+++ geolocation/js/haversine_main.js
@@ -1,4 +1,4 @@
-function calculateDistance(lat, lon){
+function calculateDistances(lat, lon){
     let dpulze = haversine(lat,lon,2.922561,101.650965)
     let pyramid = haversine(lat,lon,3.073065,101.607787)
     let klcc = haversine(lat,lon,3.158761,101.714524)
@@ -19,13 +19,13 @@ ellocate.addEventListener("click",function(){
             let userLat = postion.coords.latitude
             let userLong = position.coords.longitude
 
-            let distance = calculateDistances(userLat, userLong)
+            let distances = calculateDistances(userLat, userLong)
 
             ellat.innerHTML = "Your latitude: " + userLat
             ellong.innerHTML = "Your longitude " + userLong
             elDpulze.innerHTML = "Distance to Dpulze, Cyberjaya is " + distances[0] + "km"
             elsunway.innerHTML = "Distance to Sunway Pyramid is " + distances[1] + "km"
-            
+
         })
     }else{
             alert("Geolocation is not supported!")
