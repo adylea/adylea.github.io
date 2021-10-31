97da07d00b95faee4be624f6efb34feb1f096346
Adibah Solehah <adylea.solehah@gmail.com>
2021-10-31 23:56:13 +0800

    changes

---
+++ assigment04/js/count_vowel.js
@@ -0,0 +1,17 @@
+
+let vowelCount = document.getElementById("vowelCount")
+let status = document.getElementById("status")
+
+const vowel = ["a", "e", "i","o", "u"]
+
+vowelCount.addEventListener("onclick",function(){
+    for(i = o; i<vowelCount.length; i++)
+    let vowelA = ["a"]
+    let vowelE = ["e"]
+    let vowelI = ["i"]
+    let vowelO = ["o"]
+    let vowelu = ["u"]
+
+    if(vowel == vowelA, i++)
+
+}) 
